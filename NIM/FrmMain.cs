6596ed2bc0b9be15b9d5745ecd3042e9d320a35c
Francesco Alberghina <68157574+francesco-alberghina@users.noreply.github.com>
2022-04-09 17:25:16 +0200

    Add files via upload

---
+++ NIM/FrmMain.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NIM
+{
+    /**INTERAZIONE TRA INTERFACCIA GRAFICA E CLASSE**/
+    public partial class FrmMain : Form
+    {
+        Nim gioco;
+        int biglie, max_mosse;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenera_Click(object sender, EventArgs e)
+        {
+            //codice per generare i bottoni
+            if (nudNumeroBiglie.Value > 0 && nudMax.Value > 0)
+            {
+                nudMax.Enabled = false;
+                nudNumeroBiglie.Enabled = false;
+                btnGenera.Enabled = false;
+                biglie = (int)nudNumeroBiglie.Value;
+                max_mosse = (int)nudMax.Value;
+                //creo l'oggetto per la gestione del gioco
+                gioco = new Nim(biglie, max_mosse);
+                GeneraBottoni(biglie);
+                AggiornaTabella();
+
+
+              
+            }
+            else
+                MessageBox.Show("Assicurarsi che entrambi valori siano superiori a zero!", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void AggiornaTabella()
+        {
+            int k = 1;
+            int j = 0;
+            flpBigliettini.Controls.Clear();
+            for (int i = 0; i < biglie * max_mosse; i++)
+            {
+                Button btn = new Button();
+
+                // diamo uno stile "flat" al pulsante
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 5;
+                btn.Margin = new Padding(2);
+
+                btn.Height = (flpBigliettini.Height - (gioco.BiglieIniziali - 2) * 4) / (max_mosse);
+                btn.Width = (flpBigliettini.Width - (gioco.BiglieIniziali - 2) * 4 - 9) / (biglie);
+
+                btn.Text = gioco.tab[k, j].ToString();
+                if (btn.Text == "0")
+                {
+                    btn.BackColor = BackColor;
+                    btn.ForeColor = BackColor;
+                    btn.Text = "";
+                }
+                else
+                {
+                    btn.BackColor = Color.Gray;
+                    btn.ForeColor = Color.White;
+                }
+                j++;
+                if ((i + 1) % gioco.BiglieIniziali == 0 && i != 0)
+                {
+                    if (k < gioco.MaxMosse)
+                        k++;
+                    j = 0;
+                }
+                btn.TabIndex = i + 1;
+
+                //aggancio l'evento click al bottone. 
+                //ad ogni bottono creato è collegato lo stesso metodo di risposta al click, e il mouse che entra e esce dal controllo
+                btn.Click += Btn_Click;
+                btn.MouseEnter += MouseEntra;
+
+                btn.MouseLeave += MouseEsce;
+
+
+
+                flpBigliettini.Controls.Add(btn); //inserisco il bottone nel flowlayoutpanel
+
+            }
+        }
+
+        private void GeneraBottoni(int biglie)
+        {
+            //genera i bottoni nell'interfaccia grafica
+
+            /**SPAWN BOTTONI**/
+            for (int i = 0; i < biglie; i++)
+            {
+                Button btn = new Button();
+               
+                // diamo uno stile "flat" al pulsante
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 5;
+                btn.Margin = new Padding(2);
+
+                btn.BackColor = Color.Red;
+                btn.ForeColor = Color.White;
+
+                btn.Height = flpNim.Height-5;
+                btn.Width = (flpNim.Width - (gioco.BiglieIniziali-2)*4-9)/(biglie);
+
+                //aggancio l'evento click al bottone. 
+                //ad ogni bottono creato è collegato lo stesso metodo di risposta al click, e il mouse che entra e esce dal controllo
+                btn.Click += Btn_Click;
+                btn.MouseEnter += MouseEntra;
+                btn.MouseLeave += MouseEsce;
+
+                btn.TabIndex = i+1;
+                
+                flpNim.Controls.Add(btn); //inserisco il bottone nel flowlayoutpanel
+               
+            }
+        }
+
+        /// <summary>
+        /// Quando il mouse esce da un bottone cioè bicchiere
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MouseEsce(object sender, EventArgs e)
+        {
+            foreach(Button b in flpNim.Controls)
+            {
+                b.BackColor = Color.Red;
+            }
+            
+        }
+
+        /// <summary>
+        /// Quando il mouse entra in un bottone cioè bicchiere
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MouseEntra(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;  //il sender è il bottone "mandante", quello su cui sono
+            int bottone = b.TabIndex;
+
+            if (bottone > (biglie - max_mosse)) //se è tra quelli possibili 
+            {
+                bool trovato = false;
+                foreach (Button btn in flpNim.Controls)
+                {
+                    if(btn.TabIndex == bottone || trovato)
+                    {
+                        btn.BackColor = Color.Gray;
+                        trovato = true;
+                    }
+                    
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// Quando si clicca per ELIMINARE
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.BackColor == Color.Gray)
+            {
+                int i = 0;
+                foreach (Button b in flpNim.Controls)
+                {
+                    if (b.BackColor == Color.Gray && b.Visible!=false)
+                    {
+                        b.Visible = false;
+                        i++;
+                    }
+                }
+
+                if (gioco.MossaUtente(i))
+                {              
+                    MessageBox.Show("Hai perso");
+                    btnRestart.Enabled = true;
+                    flpNim.Enabled = false;
+                }
+                else
+                {
+                    biglie = gioco.NumeroBiglie; //decrementa il numero di biglie nell'oggetto
+                    int risultato= gioco.MossaMacchina();
+                    MessageBox.Show($"Mossa: {risultato},Caso: {biglie}");
+                    int index;
+                    int k = gioco.MaxMosse;
+                    index = (k - 1) * gioco.BiglieIniziali + biglie - 1;
+                    do
+                    {
+                        index = (k-1) * gioco.BiglieIniziali + biglie - 1;
+                    } while (k > 0 && gioco.tab[k--, biglie - 1] != risultato);
+                    if (index < 0)
+                        index = 0;
+                    flpBigliettini.Controls[index].BackColor = Color.Yellow;
+                    if(risultato==-1)//la macchina ha perso
+                    {
+                        MessageBox.Show("Hai VINTO");
+                        btnRestart.Enabled = true;
+                        flpNim.Enabled = false;
+                        gioco.RimuoviMossaSbagliata(); //elimina la mossa sbagliata
+                        //flpNim.Controls.Clear();
+                    }
+                    else
+                    {
+                        foreach (Button b in flpNim.Controls)
+                        {
+                            if (b.TabIndex > biglie-risultato)
+                            {
+                                b.Visible = false;
+                            }
+                        }
+                        biglie = gioco.NumeroBiglie; //decrementa il numero di biglie nell'oggetto
+
+                    }
+                }
+            }
+                
+        }
+
+        private void iNFOCREATORIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Un'idea di Luca Battistin\nSingh Sukhpreet\nSbalchiero Giovanni\nAlberghina Francesco\nPiccoli Mattia");
+        }
+
+        private void comeGiocareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            /**valori azzerati e far scomparire i bottoni**/
+
+            gioco.Restart();
+            biglie = gioco.NumeroBiglie;
+            flpNim.Enabled = true;
+
+            foreach (Button b in flpNim.Controls)
+            {
+                b.Visible= true;
+            }
+            
+            nudMax.Value = gioco.MaxMosse;
+            nudNumeroBiglie.Value = gioco.NumeroBiglie;
+            AggiornaTabella();
+        }
+    }
+}
---
+++ NIM/Nim.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NIM
+{
+    public class Nim
+    {
+        /*
+        ____________________________________________________________________________________       
+           _____ _____ ____   _____ ____       _____  ______ _           _   _ _____ __  __ 
+          / ____|_   _/ __ \ / ____/ __ \     |  __ \|  ____| |         | \ | |_   _|  \/  |
+         | |  __  | || |  | | |   | |  | |    | |  | | |__  | |         |  \| | | | | \  / |
+         | | |_ | | || |  | | |   | |  | |    | |  | |  __| | |         | . ` | | | | |\/| |
+         | |__| |_| || |__| | |___| |__| |    | |__| | |____| |____     | |\  |_| |_| |  | |
+          \_____|_____\____/ \_____\____/     |_____/|______|______|    |_| \_|_____|_|  |_|
+        ------------------------------------------------------------------------------------                                                                            
+        */
+        //BiglieIniziali=numero di biglie all'inizio della partita
+        //BiglieAttuali=numero di biglie che variano man a mano
+        int biglieIniziali, biglieAttuali, maxMossa, ultimaScelta, ultimoCaso; 
+        public int[,] tab;
+
+        public Nim(int n, int max)
+        {
+            this.biglieIniziali = this.biglieAttuali = n;
+            this.maxMossa = max;
+            tab = new int[max + 1, n];
+            /*
+            COMPILAZIONE DELLA TABELLA:
+            {1,2,3,3,3,3,3,3,3,3} riga che contiene il numero di mosse possibili per ogni bicchiere
+            {1,1,1,1,1,1,1,1,1,1} bigliettino 1
+            {0,2,2,2,2,2,2,2,2,2} bigliettino 2               
+            {0,0,3,3,3,3,3,3,3,3} bigliettino 3
+            */
+            int k = 1;
+            for (int c = 0; c < n; c++)
+            {
+                tab[0, c] = k;
+                for (int i = 1; i <= k; i++)
+                    tab[i, c] = i;
+
+                if (k < max)
+                    k++;
+            }
+        }
+        /// <summary>
+        /// Metodo che fa la mossa da parte dell'utente
+        /// </summary>
+        /// <returns><c>true</c>, se l'utente perde, <c>false</c> se l'utente non perde.</returns>
+        /// <param name="mossa">numero di bicchieri da togliere (da 1 a maxMossa).</param>
+        public bool MossaUtente(int mossa)
+        {
+            if (mossa <= maxMossa && mossa > 0)
+            {
+                bool perso = false;
+                biglieAttuali -= mossa;
+                if (biglieAttuali <= 0)
+                    perso = true;
+                return perso; //perde l'utente
+            }
+            else throw new Exception("Mossa non valida");
+        }
+        /// <summary>
+        /// Metodo che fa la mossa della macchina
+        /// </summary>
+        /// <returns><c>true</c>, se la macchina perde, <c>false</c> se la macchina non perde</returns>
+        public int MossaMacchina()
+        {
+            int perso;
+            int mossa;
+            Random rnd = new Random();
+            mossa = tab[rnd.Next(1, tab[0, biglieAttuali - 1]), biglieAttuali - 1];
+            if (tab[0, biglieAttuali - 1] != 1)
+            {
+                ultimaScelta = mossa;
+                ultimoCaso = biglieAttuali;
+            }
+            biglieAttuali -= mossa;
+            if (biglieAttuali == 0)
+                perso = -1; //perde la macchina
+            else
+                perso = mossa;
+            return perso;
+        }
+        /// <summary>
+        /// Metodo per rimuovere la mossa che ha fatto perdere la macchina
+        /// </summary>
+        public void RimuoviMossaSbagliata()
+        {
+            if (ultimaScelta != 0)
+            {
+                //shifto le mosse in su di 1
+                tab[ultimaScelta, ultimoCaso - 1] = 0;
+                for (int i = 1; i < maxMossa + 1; i++)
+                {
+                    if (tab[i - 1, ultimoCaso - 1] == 0)
+                    {
+                        tab[i - 1, ultimoCaso - 1] = tab[i, ultimoCaso - 1];
+                        tab[i, ultimoCaso - 1] = 0;
+                    }
+                }
+                tab[0, ultimoCaso - 1] -= 1;
+            }
+        }
+        /// <summary>
+        /// Metodo che riporta il gioco alla situazione iniziale, preservando però la matrice con le mosse
+        /// </summary>
+        public void Restart()
+        {
+            biglieAttuali = biglieIniziali;
+            ultimaScelta = 0;
+        }
+
+        /// <summary>
+        /// Proprietà che restituisce il numero di bicchieri presenti
+        /// </summary>
+        /// <value>Numero di bicchieri</value>
+        public int NumeroBiglie
+        {
+            get => biglieAttuali;
+        }
+        public int BiglieIniziali
+        {
+            get => biglieIniziali;
+        }
+        public int MaxMosse => maxMossa;
+
+    }
+}
